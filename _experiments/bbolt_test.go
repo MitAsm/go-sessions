126798e981f94384b5848d11f7c4e5e00298b6c9
MitAsm <57670844+MitAsm@users.noreply.github.com>
2019-11-12 17:33:43 +0000

    bolt update

--- _experiments/bbolt_test.go
+++ _experiments/bbolt_test.go
@@ -1,3 +1,8 @@
+/*
+	go test -bench=Insert -benchmem -benchtime 1000x -count 3 -cpu 1,2,4,8
+	go test -bench=Find -benchmem -benchtime 100000x -count 3 -cpu 1,2,4,8
+*/
+
 package bench_bolt
 
 import (
@@ -9,27 +14,57 @@ import (
 	"sync"
 	"testing"
 
-	"go.etcd.io/bbolt"
+	bolt "go.etcd.io/bbolt"
 )
 
-var working_Directory string
-var bolt_db *bbolt.DB
-var bolt_bucketName = []byte("scs:session")
+var workingDirectory string
+var boltDB *bolt.DB
+var boltBucket = []byte("sessions")
+
+const storedValueSize int = 256
 
 func init() {
 	var err error
 
-	working_Directory = os.TempDir() + "\\experiments"
-	err = os.MkdirAll(working_Directory, 0664)
+	workingDirectory = os.TempDir() + "\\experiments"
+	err = os.MkdirAll(workingDirectory, 0664)
 	if err != nil {
 		fmt.Println("Unable to create session folder!")
 		panic(err)
 	}
-	fmt.Printf("%-32s %s\n", "| Working Directory:", working_Directory)
+	fmt.Printf("%-32s %s\n", "| Working Directory:", workingDirectory)
 
-	createBoltDB()
 }
 
+func createNewDB() {
+	var err error
+
+	dropDB()
+
+	// Establish a connection to Bolt database
+	boltDB, err = bolt.Open(workingDirectory+"\\bolt.db", 0600, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create bucket
+	err = boltDB.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists(boltBucket)
+		return err
+	})
+}
+
+func dropDB() {
+	// Close current connection handle
+	if boltDB != nil {
+		_ = boltDB.Close()
+	}
+
+	// Drop database file
+	os.Remove(workingDirectory + "\\bolt.db")
+}
+
+// Generate test base64 string value
 func generateTestValue(size int) (string, error) {
 	b := make([]byte, size)
 	_, err := rand.Read(b)
@@ -39,38 +74,17 @@ func generateTestValue(size int) (string, error) {
 	return base64.RawURLEncoding.EncodeToString(b), nil
 }
 
-func createBoltDB() {
-	var err error
-
-	if bolt_db != nil {
-		_ = bolt_db.Close()
-	}
-
-	os.Remove(working_Directory + "\\bolt.db")
-
-	// Establish a connection to Bolt database
-	bolt_db, err = bbolt.Open(working_Directory+"\\bolt.db", 0600, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	err = bolt_db.Update(func(tx *bbolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(bolt_bucketName)
-		return err
-	})
-}
-
-func insert(n int, size int) {
-	err := bolt_db.Update(func(tx *bbolt.Tx) error {
+func insert(n int) {
+	err := boltDB.Update(func(tx *bolt.Tx) error {
 		var err error
 
 		var token string
 		token = fmt.Sprintf("%032d", n)
 
 		var buf string
-		buf, err = generateTestValue(size)
+		buf, err = generateTestValue(storedValueSize)
 
-		bucket := tx.Bucket(bolt_bucketName)
+		bucket := tx.Bucket(boltBucket)
 		err = bucket.Put([]byte(token), []byte(buf))
 		return err
 	})
@@ -79,67 +93,60 @@ func insert(n int, size int) {
 		log.Fatalln(err)
 	}
 }
-func insert_loop(b *testing.B, size int) {
+func insert_loop(b *testing.B) {
 	for n := 1; n <= b.N; n++ {
-		insert(n, size)
+		insert(n)
 	}
 }
 
-var sm = &sync.Mutex{}
-var sn int
+// parallel variables
+var parallelMutex = &sync.Mutex{}
+var parallelID int
 
-func insert_paralel(b *testing.B, size int) {
-	sn = 1
+func insert_parallel(b *testing.B) {
+	parallelID = 1
 
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			sm.Lock()
-			n := sn
-			sn++
-			sm.Unlock()
+			parallelMutex.Lock()
+			n := parallelID
+			parallelID++
+			parallelMutex.Unlock()
 
-			insert(n, size)
+			insert(n)
 		}
 	})
 }
 
-func BenchmarkBoltInsert_128(b *testing.B) {
-	createBoltDB()
+func Benchmark_Insert(b *testing.B) {
+	createNewDB()
 	b.ResetTimer()
-
-	insert_loop(b, 128)
+	insert_loop(b)
+	b.StopTimer()
+	dropDB()
 }
-func BenchmarkBoltInsert_1024(b *testing.B) {
-	createBoltDB()
-	b.ResetTimer()
 
-	insert_loop(b, 1024)
-}
-func BenchmarkBoltInsertParalel_128(b *testing.B) {
-	createBoltDB()
+func Benchmark_InsertParallel(b *testing.B) {
+	createNewDB()
 	b.ResetTimer()
-
-	insert_paralel(b, 128)
-}
-func BenchmarkBoltInsertParalel_1024(b *testing.B) {
-	createBoltDB()
-	b.ResetTimer()
-
-	insert_paralel(b, 1024)
+	insert_parallel(b)
+	b.StopTimer()
+	dropDB()
 }
 
 func find(n int) {
+	var token string
 	var val []byte
-	bolt_db.View(func(tx *bbolt.Tx) error {
+	boltDB.View(func(tx *bolt.Tx) error {
 
-		var token string
 		token = fmt.Sprintf("%032d", n)
 
-		bucket := tx.Bucket(bolt_bucketName)
+		bucket := tx.Bucket(boltBucket)
 		val = bucket.Get([]byte(token))
 
 		return nil
 	})
+	//fmt.Printf("%-32s %s\n", token, val)
 
 	if val == nil {
 		log.Fatalln("not found!", n)
@@ -150,47 +157,41 @@ func find_loop(b *testing.B) {
 		find(n)
 	}
 }
-func find_paralel(b *testing.B) {
-	sn = 1
+func find_parallel(b *testing.B) {
+	parallelID = 1
 
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			sm.Lock()
-			n := sn
-			sn++
-			sm.Unlock()
+			parallelMutex.Lock()
+			n := parallelID
+			parallelID++
+			parallelMutex.Unlock()
 
 			find(n)
 		}
 	})
 }
 
-func BenchmarkBoltFind_128(b *testing.B) {
-	createBoltDB()
-	insert_loop(b, 128)
-	b.ResetTimer()
+var findInitialized bool = false
 
+func BenchmarkFind(b *testing.B) {
+	if findInitialized == false {
+		createNewDB()
+		insert_loop(b)
+	}
+	b.ResetTimer()
 	find_loop(b)
-}
-func BenchmarkBoltFind_1024(b *testing.B) {
-	createBoltDB()
-	insert_loop(b, 1024)
-	b.ResetTimer()
-
-	find_loop(b)
+	//b.StopTimer()
+	//dropDB()
 }
 
-func BenchmarkBoltFindParalel_128(b *testing.B) {
-	createBoltDB()
-	insert_loop(b, 128)
+func BenchmarkFindParallel(b *testing.B) {
+	if findInitialized == false {
+		createNewDB()
+		insert_loop(b)
+	}
 	b.ResetTimer()
-
-	find_paralel(b)
-}
-func BenchmarkBoltFindParalel_1024(b *testing.B) {
-	createBoltDB()
-	insert_loop(b, 1024)
-	b.ResetTimer()
-
-	find_paralel(b)
+	find_parallel(b)
+	//b.StopTimer()
+	//dropDB()
 }
